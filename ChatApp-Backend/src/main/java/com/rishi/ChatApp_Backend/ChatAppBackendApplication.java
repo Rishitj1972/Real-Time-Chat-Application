e88309092a2366a0533631c64f0e619b7d61f6ed
Rishi <rishitj1972@gmail.com>
2026-04-24 17:31:51 +0530

    first commit

---
+++ ChatApp-Backend/src/main/java/com/rishi/ChatApp_Backend/ChatAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rishi.ChatApp_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatAppBackendApplication.class, args);
+	}
+
+}
---
+++ ChatApp-Backend/src/test/java/com/rishi/ChatApp_Backend/ChatAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rishi.ChatApp_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
